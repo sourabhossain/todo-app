4b349a33101e7a80507c2cdd58b6abdf7b9f700b
Sourab Hossain <sourabhossain.fci@gmail.com>
2020-09-09 21:37:36 +0600

    add span

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,3 +2,12 @@
 $("li").click(function() {
 	$(this).toggleClass("completed");
 });
+
+// click on x to delete todos
+$("span").click(function(event) {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+
+	event.stopPropagation();
+});
